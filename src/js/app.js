ed3abf8690beaf69da525ff47e164c7984e9e8ab
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-11-13 07:50:40 -0600

    Major restructuring of code, image display capability added, UI design in need of update

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
-'use strict' 
+'use strict';
 
 const { log } = console;
+
 function select(selector, scope = document) {
 	return scope.querySelector(selector);
 }
@@ -9,13 +10,23 @@ function listen(event, element, callback) {
 	return element.addEventListener(event, callback);
 }
 
+function isImageFile(file) {
+  return file && file.type.startsWith('image');
+}
+
+function createImage(imageSrc) {
+  const img = document.createElement('img');
+  img.src = imageSrc;  
+  img.alt = imageSrc; // Because the photo could be anything 
+  return img;
+}
+
 /*-------------------------------------------------------------->
 	Initial Declarations 
 <--------------------------------------------------------------*/
 
 const dealerDisplay = select('.dealer-value-display');
 const dealerValueDisplay = select('.value-display');
-
 const finalResultDisplay = select('h2');
 const playerDisplay = select('.player-display');
 const playerValueDisplay = select('.player-value-display');
@@ -24,147 +35,247 @@ const hitButton = select('.hit-btn');
 const holdButton = select('.hold-btn');
 const restartButton = select('.restart-btn');
 
-const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
-const ranks = [
-		'Ace', '2', '3', '4', '5', 
-		'6', '7', '8', '9', '10', 
-		'Jack', 'Queen', 'King'
-	];
-
 /*-------------------------------------------------------------->
-  Hand Declarations 
+  Card Declarations 
 <--------------------------------------------------------------*/
 
-const player = {
-	hand: [],
-	handValue: 0,
-	aceCount: 0,
-	handDisplay: []
-};
+const cardObjects = [
+  { carddisplay: 'Ace of Hearts', cardvalue: 11, imgsrc: './src/img/hearts_ace.svg' },
+  { carddisplay: '2 of Hearts', cardvalue: 2, imgsrc: './src/img/hearts_2.svg' },
+  { carddisplay: '3 of Hearts', cardvalue: 3, imgsrc: './src/img/hearts_3.svg' },
+  { carddisplay: '4 of Hearts', cardvalue: 4, imgsrc: './src/img/hearts_4.svg' },
+  { carddisplay: '5 of Hearts', cardvalue: 5, imgsrc: './src/img/hearts_5.svg' },
+  { carddisplay: '6 of Hearts', cardvalue: 6, imgsrc: './src/img/hearts_6.svg' },
+  { carddisplay: '7 of Hearts', cardvalue: 7, imgsrc: './src/img/hearts_7.svg' },
+  { carddisplay: '8 of Hearts', cardvalue: 8, imgsrc: './src/img/hearts_8.svg' },
+  { carddisplay: '9 of Hearts', cardvalue: 9, imgsrc: './src/img/hearts_9.svg' },
+  { carddisplay: '10 of Hearts', cardvalue: 10, imgsrc: './src/img/hearts_10.svg' },
+  { carddisplay: 'Jack of Hearts', cardvalue: 10, imgsrc: './src/img/hearts_jack.svg' },
+  { carddisplay: 'Queen of Hearts', cardvalue: 10, imgsrc: './src/img/hearts_queen.svg' },
+  { carddisplay: 'King of Hearts', cardvalue: 10, imgsrc: './src/img/hearts_king.svg' },
 
-const dealer = {
-	hand: [],
-	handValue: 0,
-	aceCount: 0,
-	handDisplay: []
-};
+  { carddisplay: 'Ace of Diamonds', cardvalue: 11, imgsrc: './src/img/diamonds_ace.svg' },
+  { carddisplay: '2 of Diamonds', cardvalue: 2, imgsrc: './src/img/diamonds_2.svg' },
+  { carddisplay: '3 of Diamonds', cardvalue: 3, imgsrc: './src/img/diamonds_3.svg' },
+  { carddisplay: '4 of Diamonds', cardvalue: 4, imgsrc: './src/img/diamonds_4.svg' },
+  { carddisplay: '5 of Diamonds', cardvalue: 5, imgsrc: './src/img/diamonds_5.svg' },
+  { carddisplay: '6 of Diamonds', cardvalue: 6, imgsrc: './src/img/diamonds_6.svg' },
+  { carddisplay: '7 of Diamonds', cardvalue: 7, imgsrc: './src/img/diamonds_7.svg' },
+  { carddisplay: '8 of Diamonds', cardvalue: 8, imgsrc: './src/img/diamonds_8.svg' },
+  { carddisplay: '9 of Diamonds', cardvalue: 9, imgsrc: './src/img/diamonds_9.svg' },
+  { carddisplay: '10 of Diamonds', cardvalue: 10, imgsrc: './src/img/diamonds_10.svg' },
+  { carddisplay: 'Jack of Diamonds', cardvalue: 10, imgsrc: './src/img/diamonds_jack.svg' },
+  { carddisplay: 'Queen of Diamonds', cardvalue: 10, imgsrc: './src/img/diamonds_queen.svg' },
+  { carddisplay: 'King of Diamonds', cardvalue: 10, imgsrc: './src/img/diamonds_king.svg' },
+
+  { carddisplay: 'Ace of Clubs', cardvalue: 11, imgsrc: './src/img/clubs_ace.svg' },
+  { carddisplay: '2 of Clubs', cardvalue: 2, imgsrc: './src/img/clubs_2.svg' },
+  { carddisplay: '3 of Clubs', cardvalue: 3, imgsrc: './src/img/clubs_3.svg' },
+  { carddisplay: '4 of Clubs', cardvalue: 4, imgsrc: './src/img/clubs_4.svg' },
+  { carddisplay: '5 of Clubs', cardvalue: 5, imgsrc: './src/img/clubs_5.svg' },
+  { carddisplay: '6 of Clubs', cardvalue: 6, imgsrc: './src/img/clubs_6.svg' },
+  { carddisplay: '7 of Clubs', cardvalue: 7, imgsrc: './src/img/clubs_7.svg' },
+  { carddisplay: '8 of Clubs', cardvalue: 8, imgsrc: './src/img/clubs_8.svg' },
+  { carddisplay: '9 of Clubs', cardvalue: 9, imgsrc: './src/img/clubs_9.svg' },
+  { carddisplay: '10 of Clubs', cardvalue: 10, imgsrc: './src/img/clubs_10.svg' },
+  { carddisplay: 'Jack of Clubs', cardvalue: 10, imgsrc: './src/img/clubs_jack.svg' },
+  { carddisplay: 'Queen of Clubs', cardvalue: 10, imgsrc: './src/img/clubs_queen.svg' },
+  { carddisplay: 'King of Clubs', cardvalue: 10, imgsrc: './src/img/clubs_king.svg' },
+
+  { carddisplay: 'Ace of Spades', cardvalue: 11, imgsrc: './src/img/spades_ace.svg' },
+  { carddisplay: '2 of Spades', cardvalue: 2, imgsrc: './src/img/spades_2.svg' },
+  { carddisplay: '3 of Spades', cardvalue: 3, imgsrc: './src/img/spades_3.svg' },
+  { carddisplay: '4 of Spades', cardvalue: 4, imgsrc: './src/img/spades_4.svg' },
+  { carddisplay: '5 of Spades', cardvalue: 5, imgsrc: './src/img/spades_5.svg' },
+  { carddisplay: '6 of Spades', cardvalue: 6, imgsrc: './src/img/spades_6.svg' },
+  { carddisplay: '7 of Spades', cardvalue: 7, imgsrc: './src/img/spades_7.svg' },
+  { carddisplay: '8 of Spades', cardvalue: 8, imgsrc: './src/img/spades_8.svg' },
+  { carddisplay: '9 of Spades', cardvalue: 9, imgsrc: './src/img/spades_9.svg' },
+  { carddisplay: '10 of Spades', cardvalue: 10, imgsrc: './src/img/spades_10.svg' },
+  { carddisplay: 'Jack of Spades', cardvalue: 10, imgsrc: './src/img/spades_jack.svg' },
+  { carddisplay: 'Queen of Spades', cardvalue: 10, imgsrc: './src/img/spades_queen.svg' },
+  { carddisplay: 'King of Spades', cardvalue: 10, imgsrc: './src/img/spades_king.svg' }
+];
+
 
 /*-------------------------------------------------------------->
-	Deck Shuffle
+	Player Class
 <--------------------------------------------------------------*/
-/*
-		MY NEXT PLAN IS TO CREATE A DECK CLASS AND USE THAT AS AN OBJECT TO CARRY
-		THE IMG LOCATIONS		---			EACH GAME WILL GENERATE A NEW DECK 
-		THE ARRAY WILL BE THE ID IN THE CLASS SETTER AND THAT WILL BE USED FOR 
-		SHUFFLING THE DECK
 
-		TO DISPLAY CARDS, WILL NEED A MAP FUNCTION TO SORT THROUGH ARRAY 
-		AND CREATE IMGS WITH SOURCES (DON'T FORGET DEALER HAND REVEAL)
+class Player {
+	#hand = []; 
+	#handDisplayText = []; 
+	#handDisplayImg = [];  
+	#handValue = 0; 
+	#aceCount = 0;
 
-		CSS WILL HAVE TO BE ADJUSTED ACCORDINGLY 
+	constructor(
+		hand = [], 
+		handDisplayText = [], 
+		handDisplayImg = [], 
+		handValue = 0, 
+		aceCount = 0
+	) {
+		this.#hand = hand;
+		this.#handDisplayText = handDisplayText || [];
+		this.#handDisplayImg = handDisplayImg || [];
+		this.#handValue = handValue;
+		this.#aceCount = aceCount;
 
+		this.updateHandValue(); 
+	}
 
-		I THINK I WILL NOW DISCARD DECK FOR RESET 
-*/
-function shuffle() {
-	const deck = [...new Array(52).keys()];
-	for (let i = deck.length - 1; i > 0; i--) {
+	set hand(hand) {
+		this.#hand = hand;
+		this.updateHandValue();
+	}
+	set handDisplayText(handDisplayText) {
+		this.#handDisplayText = handDisplayText;
+	}
+	set handDisplayImg(handDisplayImg) {
+		this.#handDisplayImg = handDisplayImg;
+	}
+	set handValue(handValue) {
+		this.#handValue = handValue;
+	}
+	set aceCount(aceCount) {
+		this.#aceCount = aceCount;
+	}
+
+	get hand() {
+		return this.#hand;
+	}
+	get handDisplayText() {
+		return this.#handDisplayText;
+	}
+	get handDisplayImg() {
+		return this.#handDisplayImg;
+	}
+	get handValue() {
+		return this.#handValue;
+	}
+	get aceCount() {
+		return this.#aceCount;
+	}
+
+		addCard(card, shuffledDeck) {
+		if (!Array.isArray(this.#handDisplayText)) {
+			this.#handDisplayText = [];
+		}
+		if (!Array.isArray(this.#handDisplayImg)) {
+			this.#handDisplayImg = [];
+		}
+
+		this.#hand.push(card); 
+		this.#handDisplayText.push(card.carddisplay);  
+		this.#handDisplayImg.push(card.imgsrc); 
+
+		const index = shuffledDeck.indexOf(card); 
+		if (index !== -1) {
+			shuffledDeck.splice(index, 1); 
+		}
+
+		this.updateHandValue();  
+		this.updateAceCount(card); 
+	}
+
+	updateHandValue() {
+		let value = 0;
+		this.#aceCount = 0;
+
+		this.#hand.forEach(card => {
+			value += card.cardvalue; 
+			if (card.carddisplay.includes('Ace')) {
+				this.#aceCount++;  // Count aces
+			}
+		});
+
+		this.#handValue = value;
+		while (this.#handValue > 21 && this.#aceCount > 0) {
+			this.#handValue -= 10;
+			this.#aceCount--; 
+		}
+	}
+
+	updateAceCount(card) {
+		if (card.carddisplay.includes('Ace')) {
+			this.#aceCount++;
+		}
+	}
+}
+
+/*-------------------------------------------------------------->
+	Shuffle Function (Creates a Copy of the Deck)
+<--------------------------------------------------------------*/
+
+function shuffle(deck) {
+	const shuffledDeck = [...deck]; 
+	for (let i = shuffledDeck.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
-		[deck[i], deck[j]] = [deck[j], deck[i]];
+		[shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]]; 
 	}
-	return deck;
-}
-
-let shuffledDeck = shuffle();
-
-function getCardValue(rank) {
-	let cardValue = 0;
-	if (rank === 'Ace') {
-		cardValue = 11;
-	} else if (['Jack', 'Queen', 'King'].includes(rank)) {
-		cardValue = 10;
-	} else {
-		cardValue = parseInt(rank);
-	}
-	return cardValue;
-}
-
-function dealOne(handObj) {
-	const { hand, handDisplay } = handObj;
-	const dealtCard = shuffledDeck.splice(0, 1)[0]; 
-	if (dealtCard === undefined) { 
-		console.error("Error: The deck ran out of cards or was modified incorrectly.");
-		return; 
-	}
-	const suit = suits[Math.trunc((dealtCard) / 13)];
-	const rank = ranks[(dealtCard) % 13];
-	const cardValue = getCardValue(rank);
-
-	handObj.handValue += cardValue;
-	handObj.handDisplay.push(`${rank} of ${suit}`);
-
-	if (rank === 'Ace') {
-		handObj.aceCount++;
-	}
-
 	return shuffledDeck;
 }
 
-function aceAdjust(handObj) {
-	while (handObj.handValue > 21 && handObj.aceCount > 0) {
-		handObj.handValue -= 10;
-		handObj.aceCount--;
-	}
-}
+let shuffledDeck = shuffle([...cardObjects]);
 
-function bustCheck(handObj) {
-	if (handObj.handValue > 21) {
-			aceAdjust(handObj);  
-
-			if (handObj.handValue > 21) {  
-					if (handObj === player) {
-							finalResultDisplay.textContent = 'YOU BUSTED! YOU LOSE!';
-							hitButton.classList.remove('visible');
-							holdButton.classList.remove('visible');
-							restartButton.classList.add('visible');  
-					} else if (handObj === dealer) {
-							finalResultDisplay.textContent = 'DEALER BUSTS! YOU WIN!';
-							hitButton.classList.remove('visible');
-							holdButton.classList.remove('visible');
-							restartButton.classList.add('visible');  
-					}
-			}
-	}
-}
-
+const player = new Player();
+const dealer = new Player();
 
 function updateDisplay() {
-	playerDisplay.textContent = player.handDisplay.join(', ');
+	playerDisplay.textContent = player.handDisplayText.join(', ');
 	playerValueDisplay.textContent = `Player Hand Total: ${player.handValue}`;
-	dealerDisplay.textContent = dealer.handDisplay[0];  
+	
+	playerDisplayImages(player.handDisplayImg);
+
+	dealerDisplay.textContent = dealer.handDisplayText[0];
+}
+
+function playerDisplayImages(images) {
+	playerValueDisplay.innerHTML = '';  
+
+	images.forEach(imageSrc => {
+		let imgElement = createImage(imageSrc);
+		playerValueDisplay.appendChild(imgElement);
+	});
 }
 
 function startingDeal() {
-	shuffledDeck = shuffle(); 
+	player.addCard(shuffledDeck[0], shuffledDeck);
+	dealer.addCard(shuffledDeck[0], shuffledDeck);
+	player.addCard(shuffledDeck[0], shuffledDeck);
+	dealer.addCard(shuffledDeck[0], shuffledDeck);
 
-	for (let i = 0; i < 2; i++) {
-		dealOne(player);
-		dealOne(dealer);
-	}
-	aceAdjust(dealer);
-	aceAdjust(player);
 	updateDisplay();
-	startButton.classList.add('hidden');
-	hitButton.classList.add('visible');
-	holdButton.classList.add('visible');
-	restartButton.classList.add('hidden'); 
 }
 
 function hit(handObj) {
-	dealOne(handObj);
-	aceAdjust(handObj);
+	handObj.addCard(shuffledDeck[0], shuffledDeck);
 	bustCheck(handObj);
 	updateDisplay();
+}
+
+function dealerTurn() {
+	while (dealer.handValue < 17) {
+		hit(dealer);
+	}
+	updateDisplay(); 
+}
+
+function bustCheck(handObj) {
+	if (handObj.handValue > 21) {  
+			if (handObj === player) {
+					finalResultDisplay.textContent = 'YOU BUSTED! YOU LOSE!';
+					hitButton.classList.remove('visible');
+					holdButton.classList.remove('visible');
+					restartButton.classList.add('visible');  
+			} else if (handObj === dealer) {
+					finalResultDisplay.textContent = 'DEALER BUSTS! YOU WIN!';
+					hitButton.classList.remove('visible');
+					holdButton.classList.remove('visible');
+					restartButton.classList.add('visible');  
+			}
+	}
 }
 
 function finalResult() {
@@ -180,62 +291,65 @@ function finalResult() {
 
 	hitButton.classList.remove('visible');
 	holdButton.classList.remove('visible');
-	restartButton.classList.add('visible');  
-}
-
-
-function dealerTurn() {
-	while (dealer.handValue < 17) {
-			hit(dealer);
-	}
-	updateDisplay();
-	finalResult(); 
-	hitButton.classList.remove('visible');
-	holdButton.classList.remove('visible');
-	restartButton.classList.add('visible');
+	restartButton.classList.remove('hidden');  
 }
 
 function resetGame() {
-	player.hand = [];
-	player.handValue = 0;
-	player.aceCount = 0;
-	player.handDisplay = [];
+  player.hand = [];
+  player.handValue = 0;
+  player.aceCount = 0;
+  player.handDisplayImg = [];
+  player.handDisplayText = [];
 
-	dealer.hand = [];
-	dealer.handValue = 0;
-	dealer.aceCount = 0;
-	dealer.handDisplay = [];
+  dealer.hand = [];
+  dealer.handValue = 0;
+  dealer.aceCount = 0;
+  dealer.handDisplayImg = [];
+  dealer.handDisplayText = []; 
 
-	shuffledDeck = shuffle(); 
+  shuffledDeck = shuffle([...cardObjects]);
 
-	finalResultDisplay.textContent = '';
-	playerDisplay.textContent = '';
-	playerValueDisplay.textContent = '';
-	dealerDisplay.textContent = '';
+  finalResultDisplay.textContent = '';
+  playerDisplay.textContent = '';
+  playerValueDisplay.textContent = '';
+  dealerDisplay.textContent = '';
 
-	startButton.classList.remove('hidden');
-	hitButton.classList.remove('visible');
-	holdButton.classList.remove('visible');
-	
-	restartButton.classList.remove('visible');
-	restartButton.classList.add('hidden'); 
+  startButton.classList.remove('hidden');
+  hitButton.classList.remove('visible');
+  holdButton.classList.remove('visible');
+  restartButton.classList.add('hidden'); 
+
 }
 
 
+function startBtn() {
+	startingDeal();
+	startButton.classList.add('hidden');
+	hitButton.classList.add('visible');
+	holdButton.classList.add('visible');
+	updateDisplay();
+}
+
+function hitBtn() {
+	hit(player);
+	updateDisplay();
+}
+
+function holdBtn() {
+	dealerTurn();
+	finalResult();
+}
 
 listen('click', startButton, () => { 
-	startingDeal();
+	startBtn();
 }); 
 
 listen('click', hitButton, () => {
-	hit(player);
-	bustCheck(player);
-	console.log(dealer.handValue, dealer.handDisplay);
+	hitBtn();
 });
 
 listen('click', holdButton, () => {
-	dealerTurn();
-	console.log(dealer.handValue, dealer.handDisplay);
+	holdBtn();
 });
 
 listen('click', restartButton, () => {
