ea0426447b272c3673ede375434678c281b11e11
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-12-11 08:20:49 -0600

    Bet/bank numbers set to truncate

--- src/js/app.js
+++ src/js/app.js
@@ -294,14 +294,29 @@ function updateDisplay(player, targetUL, targetImgDis) {
   updateDisplayImages(player.handDisplayImg, targetImgDis);
 }
 
+let convert = (n) => {
+  if (n < 1e3) return n;
+  if (n >= 1e3 && n < 1e6)
+      return +(n / 1e3).toFixed(1) + "K";
+  if (n >= 1e6 && n < 1e9)
+      return +(n / 1e6).toFixed(1) + "M";
+  if (n >= 1e9 && n < 1e12)
+      return +(n / 1e9).toFixed(1) + "B";
+  if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+};
+
+
+let convertedBank = convert(playerBank);
+let convertedBet = convert(playerBetTotal);
+
 function updateBankDisplay() {
-  bankDisplay.textContent = `${playerBank}`;
-  sideBankDisplay.textContent = `${playerBank}`;
+  bankDisplay.textContent = `${convertedBank}`;
+  sideBankDisplay.textContent = `${convertedBank}`;
 }
 
 function updateTotalBet() {
-  totalPlayerBet.textContent = `${playerBetTotal}`;
-  sidePlayerBet.textContent = `${playerBetTotal}`;
+  totalPlayerBet.textContent = `${convertedBet}`;
+  sidePlayerBet.textContent = `${convertedBet}`;
 }
 
 /*-------------------------------------------------------------->
