02a87e2dd1ac48f6f22abe6502132dbf6ccee12e
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-12-16 21:13:31 -0600

    Bet area styled

--- src/js/app.js
+++ src/js/app.js
@@ -39,6 +39,18 @@ function sleep(duration) {
   });
 }
 
+
+let convert = (n) => {
+  if (n < 1e3) return n;
+  if (n >= 1e3 && n < 1e6)
+      return +(n / 1e3).toFixed(1) + "K";
+  if (n >= 1e6 && n < 1e9)
+      return +(n / 1e6).toFixed(1) + "M";
+  if (n >= 1e9 && n < 1e12)
+      return +(n / 1e9).toFixed(1) + "B";
+  if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+};
+
 /*-------------------------------------------------------------->
   Initial Declarations 
 <--------------------------------------------------------------*/
@@ -291,20 +303,9 @@ function updateDisplay(player, targetUL, targetImgDis) {
     li.textContent = text; 
     targetUL.appendChild(li); 
   });
-  updateDisplayImages(player.handDisplayImg, targetImgDis);
+  updateDisplayImages(player.handDisplayImg, targetImgDis);  
 }
 
-let convert = (n) => {
-  if (n < 1e3) return n;
-  if (n >= 1e3 && n < 1e6)
-      return +(n / 1e3).toFixed(1) + "K";
-  if (n >= 1e6 && n < 1e9)
-      return +(n / 1e6).toFixed(1) + "M";
-  if (n >= 1e9 && n < 1e12)
-      return +(n / 1e9).toFixed(1) + "B";
-  if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
-};
-console.log(typeof playerBetTotal);
 
 
 function updateBankDisplay() {
