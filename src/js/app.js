a9a013adf9199048335fa32a0618ac81e40f2bd3
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-12-19 16:14:59 -0600

    Player class placed in separate file

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,7 @@
 'use strict';
 import { cardObjects } from "./deck.js";
 import { select, listen, isImageFile, createImage, shuffle } from "./utils.js";
-
-let convert = (n) => {
-  if (n < 1e3) return n;
-  if (n >= 1e3 && n < 1e6)
-      return +(n / 1e3).toFixed(1) + "K";
-  if (n >= 1e6 && n < 1e9)
-      return +(n / 1e6).toFixed(1) + "M";
-  if (n >= 1e9 && n < 1e12)
-      return +(n / 1e9).toFixed(1) + "B";
-  if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
-};
+import { Player } from "./class.js";
 
 /*-------------------------------------------------------------->
   Initial Declarations 
@@ -48,110 +38,6 @@ const totalPlayerBet = select('.pot');
 const sidePlayerBet = select('.side-pot');
 
 
-/*-------------------------------------------------------------->
-  Player Class
-<--------------------------------------------------------------*/
-
-class Player {
-  #hand = []; 
-  #handDisplayText = []; 
-  #handDisplayImg = [];  
-  #handValue = 0; 
-  #aceCount = 0;
-
-  constructor(
-    hand = [], 
-    handDisplayText = [], 
-    handDisplayImg = [], 
-    handValue = 0, 
-    aceCount = 0
-  ) {
-    this.#hand = hand;
-    this.#handDisplayText = handDisplayText || [];
-    this.#handDisplayImg = handDisplayImg || [];
-    this.#handValue = handValue;
-    this.#aceCount = aceCount;
-
-    this.updateHandValue(); 
-  }
-
-  set hand(hand) {
-    this.#hand = hand;
-    this.updateHandValue();
-  }
-  set handDisplayText(handDisplayText) {
-    this.#handDisplayText = handDisplayText;
-  }
-  set handDisplayImg(handDisplayImg) {
-    this.#handDisplayImg = handDisplayImg;
-  }
-  set handValue(handValue) {
-    this.#handValue = handValue;
-  }
-  set aceCount(aceCount) {
-    this.#aceCount = aceCount;
-  }
-
-  get hand() {
-    return this.#hand;
-  }
-  get handDisplayText() {
-    return this.#handDisplayText;
-  }
-  get handDisplayImg() {
-    return this.#handDisplayImg;
-  }
-  get handValue() {
-    return this.#handValue;
-  }
-  get aceCount() {
-    return this.#aceCount;
-  }
-
-  addCard(card, shuffledDeck) {
-    if (!Array.isArray(this.#handDisplayText)) {
-      this.#handDisplayText = [];
-    }
-    if (!Array.isArray(this.#handDisplayImg)) {
-      this.#handDisplayImg = [];
-    }
-    this.#hand.push(card); 
-    this.#handDisplayText.push(card.carddisplay);  
-    this.#handDisplayImg.push(card.imgsrc); 
-
-    const index = shuffledDeck.indexOf(card); 
-    if (index !== -1) {
-      shuffledDeck.splice(index, 1); 
-    }
-
-    this.updateHandValue();  
-    this.updateAceCount(card); 
-  }
-
-  updateHandValue() {
-    let value = 0;
-    this.#aceCount = 0;
-
-    this.#hand.forEach(card => {
-      value += card.cardvalue; 
-      if (card.carddisplay.includes('Ace')) {
-        this.#aceCount++;  // Count aces
-      }
-    });
-
-    this.#handValue = value;
-    while (this.#handValue > 21 && this.#aceCount > 0) {
-      this.#handValue -= 10;
-      this.#aceCount--; 
-    }
-  }
-
-  updateAceCount(card) {
-    if (card.carddisplay.includes('Ace')) {
-      this.#aceCount++;
-    }
-  }
-}
 
 /*-------------------------------------------------------------->
 
@@ -164,6 +50,17 @@ const dealer = new Player();
 let playerBank = 1000;
 let playerBetTotal = 0;
 let pot = 0;
+
+let convert = (n) => {
+  if (n < 1e3) return n;
+  if (n >= 1e3 && n < 1e6)
+      return +(n / 1e3).toFixed(1) + "K";
+  if (n >= 1e6 && n < 1e9)
+      return +(n / 1e6).toFixed(1) + "M";
+  if (n >= 1e9 && n < 1e12)
+      return +(n / 1e9).toFixed(1) + "B";
+  if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+};
 
 /*-------------------------------------------------------------->
   Display Functions 
